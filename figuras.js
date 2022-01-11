3df967140ee91ced800f0a2aa20180e6d55af7a8
RenatoArenas <renato.arenas2012@gmail.com>
2022-01-11 09:55:25 -0500

    Modificado figuras.js

--- figuras.js
+++ figuras.js
@@ -1,17 +1,21 @@
 //Código de cuadrado
 console.group("Cuadrado");
 
-const lCuadrado = 3;
+const lCuadrado = 6;
+let perC = 0;
+let areaC = 0;
 
 console.log("Los lados del cuadrado valen: " + lCuadrado + "cm")
 
 function calculosCuadrado(a) {
-    const per = a * 4;
-    const area = a ** 2;
-    console.log("El perimetro del cuadrado es: " + per + "cm");
-    console.log("El área del cuadrado es: " + area + "cm^2");
+    perC = a * 4;
+    areaC = a ** 2;
+    return perC, areaC;
 }
 calculosCuadrado(lCuadrado);
+
+console.log("El perimetro del cuadrado es: " + perC + "cm");
+console.log("El área del cuadrado es: " + areaC + "cm^2");
 
 console.groupEnd();
 
@@ -22,16 +26,20 @@ const lTriangulo1 = 3;
 const lTriangulo2 = 4;
 const bTriangulo = 5;
 const hTriangulo = 5.5;
+let perT = 0;
+let areaT = 0;
 
 console.log("Los lados del triángulo valen: " + lTriangulo1 + "cm, " + lTriangulo2 + "cm, " + bTriangulo + "cm");
 
 function calculosTriangulo(l1, l2, b, h) {
-    const per = l1 + l2 + b;
-    const area = (b * h)/2;
-    console.log("El perimetro del triangulo es: " + per + "cm");
-    console.log("El área del cuadrado es: " + area + "cm^2");
+    perT = l1 + l2 + b;
+    areaT = (b * h)/2;
+    return perT, areaT;
 }
 calculosTriangulo(lTriangulo1, lTriangulo2, bTriangulo, hTriangulo);
+
+console.log("El perimetro del triangulo es: " + perT + "cm");
+console.log("El área del triangulo es: " + areaT + "cm^2");
 
 console.groupEnd();
 
@@ -39,19 +47,23 @@ console.groupEnd();
 console.group("Circulo");
 
 const radio = 2;
-const diametro = 2 * radio;
 const pi = Math.PI;
+let diam = 0;
+let perCi = 0;
+let areaCi = 0;
 
-console.log("El radio es: " + radio);
-console.log("El diametro es: " + diametro);
-
-function calculosCirculo(r, d, p) {
-    const per = d * p;
-    const area = (r**2) * p
-    console.log("El perimetro del circulo es: " + per + "cm");
-    console.log("El área del circulo es: " + area + "cm^2");
+function calculosCirculo(r) {
+    diam = r * 2;
+    perCi = diam * pi;
+    areaCi = (r**2) * pi;
+    return perCi, areaCi;
 }
 
-calculosCirculo(radio, diametro, pi);
+calculosCirculo(radio);
+
+console.log("El radio es: " + radio);
+console.log("El diametro es: " + diam);
+console.log("El perimetro del circulo es: " + perCi + "cm");
+console.log("El área del circulo es: " + areaCi + "cm^2");
 
 console.groupEnd();
